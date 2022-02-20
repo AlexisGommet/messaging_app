1fae2779ced2d72dbf7a6bb243e745e322595223
Alexis Gommet <sherman503@hotmail.fr>
2022-02-20 02:41:10 +0100

    Fix scroll to bottom and comments

--- src/App.js
+++ src/App.js
@@ -22,12 +22,13 @@ firebase.initializeApp({
     measurementId: "G-QRKBHTYSCJ"
 })
 
-// TODO : commentez code et si messages < 5 min, enlevez photo pour messages suivants et les rapprochez
+// TODO : Enlevez photo pour messages suivants (5 min) et les rapprochez
 
 const analytics = getAnalytics();
 const auth = getAuth();
 const firestore = getFirestore();
 
+// Get height and width for landscape mode
 const appHeight = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
 const landscape = () => document.documentElement.style.setProperty('--app-width', `${window.innerWidth}px`)
 window.addEventListener('resize', appHeight);
@@ -66,23 +67,23 @@ function SignIn() {
 
 function SignOut(props) {
 
-  const prop = props;
-
   const[loading, setLoading] = useState(true);
 
   const memes = ["Loading dank memes", "Invoking dark magic", "very science, wow much application, so technology", "beep boop beep plotting destruction of humanity"];
   const screen = ["spinningBubbles", "balls", "bars", "bubbles", "cubes", "cylon", "spin", "spokes"];
 
+  // Show loading screen then the sign out button and the chatroom and scroll to bottom 
   useEffect(() => {
     setTimeout(() => {
       setLoading();
-      prop.update();  
+      props.update();  
+      props.scroll();
     }, 4000);
   }, [])
 
   return auth.currentUser && (
     <>
-      {loading ? <> <ReactLoading type={screen[Math.floor(Math.random() * 8)]} color={"#ffffff"} height={200} width={200} /> <br></br> <p>{memes[Math.floor(Math.random() * 4)]}</p> </>
+      {loading ? <> <ReactLoading type={screen[Math.floor(Math.random() * screen.length)]} color={"#ffffff"} height={200} width={200} /> <br></br> <p>{memes[Math.floor(Math.random() * memes.length)]}</p> </>
       :
       <>
         <div className='top'><p>Not Messenger</p>
@@ -100,16 +101,18 @@ function Chat() {
   const anchor = useRef();
   const listRef = useRef();
   const previousScrollDiff = useRef(0);
-  const canload = useRef(false);
+  const canload = useRef(true);
   const scrollonload = useRef(false);
+  const snapshot = useRef(false);
 
   const [loading, setLoading] = useState(false); 
+  // Load more messages for tall screens else the loading of more messages on scroll to top will break
   const [loadmore, setLoadMore] = useState(window.innerHeight > 1100 ? 30 : 15);
   
-
+  // Get collection for this chat
   const [querySnapshot] = useCollectionData(query(collection(firestore, "Chat_general"), orderBy("createdAt"), limitToLast(loadmore)));
 
-  // TODO : commentez code et si messages < 5 min, enlevez photo pour messages suivants et les rapprochez
+  // TODO : Enlevez photo pour messages suivants (5 min) et les rapprochez
 
   let day = "0";
   const setDay = (newday) => {day = newday;}
@@ -124,14 +127,21 @@ function Chat() {
   const getHourf = () => {return hourf;}
   const setHourf = (newhour) => {hourf = newhour;}
 
+  // Scroll to bottom on other people's messages
   const scrollonsnap = onSnapshot(collection(firestore, "Chat_general"), () => {
     if (anchor.current && canload.current) {
+      // Deactivate instant scroll to top position if it's a snapshot for 100 ms
+      snapshot.current = true;
+      setTimeout(() => {
+        snapshot.current = false;  
+      }, 100);
       anchor.current.scrollIntoView({ behavior: 'smooth' });
     }
   });
 
   const sendMessage = async (e,resetform,formValue) => {
-
+    
+    // Prevent refresh of the page on form submit
     e.preventDefault();
 
     await addDoc(collection(firestore, "Chat_general"), {
@@ -143,41 +153,45 @@ function Chat() {
     resetform();
   }
 
-  // TODO : commentez code et si messages < 5 min, enlevez photo pour messages suivants et les rapprochez
+  // TODO : Enlevez photo pour messages suivants (5 min) et les rapprochez
 
+  // To not scroll instantly to bottom on first render
   useEffect(() => {
     setTimeout(() => {
       scrollonload.current = true;  
-    }, 1000);
+    }, 4100);
   }, [])
 
+  // 100 ms buffer to not scroll to bottom on re-render for loading more messages
   useEffect(() => {
     canload.current = false; 
     setTimeout(() => {
       canload.current = true;  
     }, 100);
+    // Set previous scroll height before loading messages, to give impression that window didn't move and just expanded upward
+    if(listRef.current && scrollonload.current && !snapshot.current){
+      listRef.current.scroll({
+        top: listRef.current.scrollHeight - previousScrollDiff.current,
+        left: 0,
+        behavior: 'instant'
+    });
+    }
   })
 
+  // If user scroll to the top, get actual scroll height and load more messages
   const load = (scrollTop, scrollHeight) => {  
-    if(scrollHeight - scrollTop > scrollHeight * 0.98 && canload.current && loadmore <= querySnapshot.length){
+    if(scrollHeight - scrollTop > scrollHeight * 0.98 && canload.current && loadmore <= querySnapshot.length && scrollonload.current){
       previousScrollDiff.current = listRef.current.scrollHeight - listRef.current.scrollTop;
       setLoadMore(loadmore + 10);  
     } 
   }
 
-  // TODO : commentez code et si messages < 5 min, enlevez photo pour messages suivants et les rapprochez
+  // TODO : Enlevez photo pour messages suivants (5 min) et les rapprochez
 
+  // Props for SignOut
   let upstate = () => {setLoading(true)}
 
-   useEffect(()=>{
-    if(listRef.current && scrollonload.current){
-      listRef.current.scroll({
-        top: listRef.current.scrollHeight - previousScrollDiff.current,
-        left: 0,
-        behavior: 'instant'
-      });
-    }
-   })
+  let scroll = () => {anchor.current.scrollIntoView({ behavior: 'smooth' });}
 
   return (
     <>
@@ -190,7 +204,7 @@ function Chat() {
           <Form send_msg={sendMessage}/>
         </>
         : <></>}
-      <SignOut update={upstate}/>
+      <SignOut update={upstate} scroll={scroll}/>
     </>
   );
 }
@@ -212,7 +226,11 @@ return (
 
 function ChatMessage(props) {
   
+  // Give message proper css class if the current user sent it or not
   const messageClass = props.message.user === auth.currentUser.uid ? 'sent' : 'received';
+
+  // Manipulation of createdAt property to know if a day or hour timestamp should be displayed
+  // ******************************************************************************************************* //
   let hour = "";
   let dayint2 = "";
   let newdaystr = "";
@@ -229,12 +247,15 @@ function ChatMessage(props) {
   const dayint = props.di();
   const day = props.d();
   const hourf = parseFloat(props.hf());
-  
+
+  // Display day every new day
   const newday = (day === current_day) || (day < current_day && dayint !== dayint2);
+  // Display hour every 5 min only
   const newhour =  hourf + 300 <= parseFloat(current_day) || newday || hourf === parseFloat(current_day);
 
   if(newday){props.ds(current_day);props.dis(dayint2);}
   if(newhour){props.hfs(current_day);}
+  // ******************************************************************************************************* //
 
   return (
     <>
